be3e094b3dd57792df024829d4de3cc5484738bc
Atilas Rodrigues Lamberti <atilas.lamberti@lais.huol.ufrn.br>
2024-07-01 09:49:43 -0300

    Sessão Templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Atilas Rodrigues Lamberti',
+        'recipes': [make_recipe() for _ in range(10)],
+    })
+
+def repice(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
